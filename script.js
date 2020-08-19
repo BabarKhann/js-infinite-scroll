5028289ecd0177a301a8da9829b033c1665d0935
Babar Khan <babarkhan138@gmail.com>
2020-08-19 08:56:34 +0500

    Load images on scroll before the last image

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 const imageContainer = document.getElementById("image-container");
 const loader = document.getElementById("loader");
 
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 let photos = [];
 
 // Unsplash API
-const count = 10;
+const count = 20;
 const apiKey = "3lgeIYFtrf9GeSUdsamL9e6aD_CanlDLPYSpa0ITkVE";
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+function imageLoaded() {
+  console.log("Image Loaded");
+  imagesLoaded++;
+
+  if (imagesLoaded === totalImages) {
+    ready = true;
+    console.log("ready =", ready);
+  }
+}
 
 // Helper Function to Set Attributes on DOM Elements
 function setAttributes(element, attributes) {
@@ -17,6 +30,8 @@ function setAttributes(element, attributes) {
 
 // Create Elements For Links & Photos, Add to DOM
 function displayPhotos() {
+  totalImages = photos.length;
+  console.log("totalImages =", totalImages);
   // Run function for each object in photos
   photos.forEach((photo) => {
     //   Create <a> link to Unsplash
@@ -32,6 +47,8 @@ function displayPhotos() {
       alt: photo.alt_description,
       title: photo.alt_description,
     });
+    // Event Listener, check when each is finished loading
+    img.addEventListener("load", imageLoaded);
     //  Put <img> inside <a></a>, then put both inside in imageContainer Element
     item.appendChild(img);
     imageContainer.appendChild(item);
@@ -47,5 +64,18 @@ async function getPhotos() {
   } catch (error) {}
 }
 
+// Check to see if scrolling near bottom of page, Load More Photos
+
+window.addEventListener("scroll", () => {
+  if (
+    window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 &&
+    ready
+  ) {
+    ready = false;
+    imagesLoaded = 0;
+    getPhotos();
+  }
+});
+
 // On Load
-getPhotos();
+// getPhotos();
